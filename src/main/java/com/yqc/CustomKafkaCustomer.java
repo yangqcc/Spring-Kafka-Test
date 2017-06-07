e3737c0c38d4d960ee2048c62abe3d0d40c0bb36
yangqc <yangqc_ars@outlook.com>
2017-06-07 23:22:39 +0800

    Initial commit

---
+++ src/main/java/com/yqc/CustomKafkaCustomer.java
@@ -0,0 +1,7 @@
+package com.yqc;
+
+/**
+ * Created by yangqc on 2017/6/7.
+ */
+public class CustomKafkaCustomer {
+}
---
+++ src/main/java/com/yqc/CustomKafkaProducer.java
@@ -0,0 +1,28 @@
+package com.yqc;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * Created by yangqc on 2017/6/7.
+ */
+public class KafkaProducer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
+        producer.close();
+    }
+}
