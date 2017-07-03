a08e3e536a6278ad1a029a38a14d8b9f6740efaa
yangqc <yangqc_ars@outlook.com>
2017-07-03 23:50:00 +0800

    kafka客户端每个分区手动提交

---
+++ src/main/java/com/yqc/KafkaPositoinManualCommit.java
@@ -0,0 +1,50 @@
+package com.yqc;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by yangqc on 2017/7/3.
+ */
+public class KafkaPositoinManualCommit {
+
+    private static AtomicBoolean running = new AtomicBoolean(true);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "false");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar"));
+        try {
+            while (running.get()) {
+                ConsumerRecords<String, String> records = consumer.poll(Long.MAX_VALUE);
+                //每个分区分开提交
+                for (TopicPartition partition : records.partitions()) {
+                    List<ConsumerRecord<String, String>> partitionRecords = records.records(partition);
+                    for (ConsumerRecord<String, String> record : partitionRecords) {
+                        System.out.println(record.offset() + ": " + record.value());
+                    }
+                    long lastOffset = partitionRecords.get(partitionRecords.size() - 1).offset();
+                    consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(lastOffset + 1)));
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+}
