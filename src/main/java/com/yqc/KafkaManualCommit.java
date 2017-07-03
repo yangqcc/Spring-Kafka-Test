c8378e4777958932bcd021adde6bd1dfbb73dbec
yangqc <yangqc_ars@outlook.com>
2017-07-03 23:46:39 +0800

    kafka客户端手动提交

---
+++ src/main/java/com/yqc/KafkaManualCommit.java
@@ -0,0 +1,44 @@
+package com.yqc;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by yangqc on 2017/7/3.
+ */
+public class KafkaManualCommit {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "false");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar"));
+        final int minBatchSize = 3;
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                buffer.add(record);
+            }
+            if (buffer.size() >= minBatchSize) {
+                System.out.println(records.count());
+                //do other action
+                consumer.commitSync();
+                buffer.clear();
+                System.out.println("commit!");
+            }
+        }
+    }
+}
