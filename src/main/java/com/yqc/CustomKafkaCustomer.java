05d7ba66bd8e4aab0b0c1720c934122765243155
yangqc <yangqc_ars@outlook.com>
2017-07-20 10:40:23 +0800

    多个客户端订阅同一个topic

--- src/main/java/com/yqc/CustomKafkaCustomer.java
+++ src/main/java/com/yqc/CustomKafkaCustomer.java
@@ -6,31 +6,46 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * Created by yangqc on 2017/6/7.
  */
 public class CustomKafkaCustomer {
     public static void main(String[] args) throws InterruptedException {
-        Properties props = new Properties();
+        Executor executor = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            executor.execute(new MyRunnable(Integer.toString(i)));
+        }
+    }
+}
+
+class MyRunnable implements Runnable {
+
+    private Properties props = new Properties();
+    private KafkaConsumer<String, String> consumer;
+    private String id;
+
+    public MyRunnable(String id) {
+        this.id = id;
         props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "test");
+        props.put("group.id", this.id);
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("foo", "bar"));
+        consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo"));
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "---启动!");
         while (true) {
-            int i = 0;
-            ConsumerRecords<String, String> records = consumer.poll(5000);
-            System.out.println("polled!");
+            ConsumerRecords<String, String> records = consumer.poll(1000);
             for (ConsumerRecord<String, String> record : records) {
-                i++;
-                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
-           /*     if (i == records.count()) {
-                    Thread.sleep(100000);
-                }*/
+                System.out.printf("%s,offset = %d, key = %s, value = %s%n", Thread.currentThread().getName(), record.offset(), record.key(), record.value());
             }
         }
     }
